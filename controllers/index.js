f0a65a082aececb073550313f6ec7f10b2203d50
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-06-20 16:47:58 -0500

    Added modules required for handlebars, added basic folder structure for front-end template generaton, created test rout for display in controllers>index.js and added template engine in main server.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const list = require('../models/List');
+const Payment = require('../models/Payment');
+const User = require('../models/User');
+const UserList = require('../models/UserList');
+
 router.use('/api', apiRoutes);
+
+router.get('/',  (req,res) => {
+    res.render('main')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
