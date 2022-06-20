4106c8e1338f96ce4d3326346c8edc79c141c788
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-06-20 18:12:30 -0500

    Added test front-end test script and css

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
 // Add front end JavaScript here
+
+const homeBtn = document.querySelector('#homeBtn');
+const dashBtn = document.querySelector('#dashBtn');
+const loginBtn = document.querySelector('#loginBtn');
+const signupBtn = document.querySelector('#signupBtn');
+
+homeBtn.addEventListener('click', () => {
+    console.log('Home button')
+});
+
+dashBtn.addEventListener('click', () => {
+    console.log('Dashboard button')
+});
+
+loginBtn.addEventListener('click', () => {
+    console.log('Login button')
+});
+
+signupBtn.addEventListener('click', () => {
+    console.log('Sign up button')
+})
