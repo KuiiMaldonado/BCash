ef3f169994665603d1ee0007b73d31e5f5af81a4
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-06-20 20:23:56 -0500

    lesdebo route implemented

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -32,4 +32,15 @@ router.get('/medeben', authenticateToken, async (req, res) => {
     }
 });
 
+router.get('/lesdebo', authenticateToken, async (req, res) => {
+    try {
+        const [lists] = await sequelize.query(`select distinct listId from payment where debtorId = ${req.user.userId};`);
+        console.log(lists);
+        res.status(200).json(lists);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
